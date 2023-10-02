b70ba65c52ea7506ebbb550ff7f74fe1c7c458b2
Nargiza Nematova <nnematova7250@gmail.com>
2023-10-02 18:11:04 +0500

    added WebSocket

--- src/main/java/com/example/web/config/WebSocketConfig.java
+++ src/main/java/com/example/web/config/WebSocketConfig.java
@@ -2,7 +2,6 @@ package com.example.web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -14,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("websocket-connection").withSockJS();
+        registry.addEndpoint("/websocket-connection").withSockJS();
     }
 
     @Override
